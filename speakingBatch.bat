48afc57a0f0a58337b4b7802b3b640ee34f421e0
Nicholas Dru <nicholasdru@hotmail.com>
2019-06-09 20:44:46 -0300

    Ajout de la couleur verte a ma console du programme speakingBatch

--- speakingBatch.bat
+++ speakingBatch.bat
@@ -1,7 +1,7 @@
 @echo off
 echo set speech = Wscript.CreateObect("SAPI.spVoici") >> "temp.vbs"
 set text = Welcome to my speaking batch file!
-
+color 2
 echo speech.speak "%text%" >> "temp.vbs"
 start temp.vbs
 pause
